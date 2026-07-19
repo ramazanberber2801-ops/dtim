b0839491c0a2ec033f14b0c4ba25f2d50b7c0617
ramazanberber2801-ops <ramazanberber2801@gmail.com>
2026-07-19 22:44:18 +0300

    Route public member card links from organization home

--- src/pages/PublicOrganizationHome.tsx
+++ src/pages/PublicOrganizationHome.tsx
@@ -1,7 +1,10 @@
 import { PublicMembershipRequest } from '../components/PublicMembershipRequest';
 import { DEFAULT_ORGANIZATION_ID } from '../lib/organization';
+import { PublicMemberCardPage } from './PublicMemberCardPage';
 import { PublicOrganizationPage } from './PublicOrganizationPage';
 
 export function PublicOrganizationHome(){
-  return <div><PublicOrganizationPage/><div className="mx-auto -mt-24 max-w-4xl px-4 pb-28"><PublicMembershipRequest organizationId={DEFAULT_ORGANIZATION_ID}/></div></div>;
+ const path=window.location.pathname.replace(/\/+$/,'')||'/';
+ if(path==='/member-card')return <PublicMemberCardPage/>;
+ return <div><PublicOrganizationPage/><div className="mx-auto -mt-24 max-w-4xl px-4 pb-28"><PublicMembershipRequest organizationId={DEFAULT_ORGANIZATION_ID}/></div></div>;
 }
